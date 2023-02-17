882c53efc144e96797b249e434c001e2698521f1
anso33 <hyeon131812@gmail.com>
2023-02-18 00:21:10 +0900

    FIX: add annotation

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,6 +3,7 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class UserController {
     users.add(newUser);
   }
 
+  @GetMapping("/user")
   public List<UserResponse> getUsers() {
     List<UserResponse> responses = new ArrayList<>();
     for (int i = 0; i < users.size(); i++) {
