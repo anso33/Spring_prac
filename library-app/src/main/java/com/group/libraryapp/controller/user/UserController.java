c2e6e8b1a499f6decd83c834ae77987caef2f731
anso33 <hyeon131812@gmail.com>
2023-02-18 01:31:03 +0900

    FEAT: use database in springboot

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,31 +3,51 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-  private final List<User> users = new ArrayList<>();
+//  private final List<User> users = new ArrayList<>(); -> 이제 DB를 사용한다.
+  private final JdbcTemplate jdbcTemplate;
+
+  public UserController(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
   @PostMapping("/user")
   public void saveUser(@RequestBody UserCreateRequest request) {
-    User newUser = new User(request.getName(), request.getAge());
-    users.add(newUser);
+    String sql = "INSERT INTO user(name, age) VALUES(?, ?)";
+    jdbcTemplate.update(sql, request.getName(), request.getAge());
   }
 
   @GetMapping("/user")
   public List<UserResponse> getUsers() {
-    List<UserResponse> responses = new ArrayList<>();
-    for (int i = 0; i < users.size(); i++) {
-      responses.add(new UserResponse(i + 1, users.get(i)));
-    }
-    return responses;
+    String sql = "SELECT * FROM user";
+//    return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+//      @Override
+//      public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+//        long id = rs.getLong("id");
+//        String name = rs.getString("name");
+//        int age = rs.getInt("age");
+//        return new UserResponse(id, name, age);
+//      }
+//    });
+    return jdbcTemplate.query(sql, (rs, rowNum) ->  {
+        long id = rs.getLong("id");
+        String name = rs.getString("name");
+        int age = rs.getInt("age");
+        return new UserResponse(id, name, age);
+    });
   }
 }
